2981b1ad50a5ae9a3a7839797af807382996e02b
dasol6063 <dasol6063@gmail.com>
2026-02-24 00:06:36 +0900

    Update week5.java

--- week5.java
+++ week5.java
@@ -2,17 +2,18 @@ package mogakso;
 
 public class mogakso_week5 {
     public static void main(String[] args){
+
         Student student1 = new Student();
         student1.studentName = "김민경";
 
         Student student2 = new Student();
-        student1.studentName = "김서진";
+        student2.studentName = "김서진";
 
         Student student3 = new Student();
-        student1.studentName = "박혜빈";
-        
-        System.out.println(student1);
-        System.out.println(student2);
-        System.out.println(student3);
+        student3.studentName = "박혜빈";
+
+        System.out.println(student1.studentName);
+        System.out.println(student2.studentName);
+        System.out.println(student3.studentName);
     }
 }
