fb2954be588a372ce2c5c5510efa011607dac9c9
dasol6063 <dasol6063@gmail.com>
2026-02-23 23:43:30 +0900

    Create week4.java

---
+++ week4.java
@@ -0,0 +1,17 @@
+package mogakso;
+
+public class mogakso_week4 {
+    public class Student{ 
+        int StudentIO;
+        String studentName;
+        int grade;
+        String address;
+        
+        public void showStudentInfo(){
+            System.out.println(studentName + "," + address);
+        }
+
+            
+    }
+    
+}
