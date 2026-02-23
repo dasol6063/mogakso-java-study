1a913b064ea9d62c315ffca0c47d151f00e6f33d
dasol6063 <dasol6063@gmail.com>
2026-02-23 23:53:49 +0900

    Create week5.java

---
+++ week5.java
@@ -0,0 +1,18 @@
+package mogakso;
+
+public class mogakso_week5 {
+    public static void main(String[] args){
+        Student student1 = new Student();
+        student1.studentName = "김민경";
+
+        Student student2 = new Student();
+        student1.studentName = "김서진";
+
+        Student student3 = new Student();
+        student1.studentName = "박혜빈";
+        
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+    }
+}
